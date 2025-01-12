b7af7480ee5c3a6c3087e376366ef052c4a500df
reall <reall@MSI>
2025-01-11 17:32:41 -0700

    Added Unit Test 1

---
+++ library/Server.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace library;
+
+public enum States
+{
+    Follower,
+    Candidate,
+    Leader
+};
+
+public class Server
+{
+    public States State = States.Follower; 
+    public int ElectionTimeout { get; set; } //Specifies the Election Timeout in milisecondss
+    public Server RecognizedLeader = new Server();
+
+    public async Task ProcessReceivedAppendEntryAsync(Server fromServer, int MilisecondsAtWhichReceived)
+    {
+        await Task.CompletedTask;
+        return;
+    }
+}
--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,10 +1,25 @@
-﻿namespace tests;
+﻿using library;
+using System.Security.Cryptography.X509Certificates;
+
+namespace tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public async Task TestElectionTimeout()
     {
+        //Given the Election Timeout is 150 miliseconds
+        //and Server L is the leader
+        Server serverL = new Server();
+        serverL.State = States.Leader;
+        Server serverA = new Server();
+        serverA.ElectionTimeout = 150;
+        serverA.RecognizedLeader = serverL;
 
+        // When Server A receives an Append entry RPC from the leader in 30 mili seconds
+        await serverA.ProcessReceivedAppendEntryAsync(serverL, 30);
+
+        //Then Server L remains the leader
+        Assert.True(serverL.State == States.Leader);
     }
 }
