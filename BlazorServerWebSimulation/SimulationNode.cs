42b6ca9b541fe3388c6f08debcc256c0a37a26c0
reall <reall@MSI>
2025-01-15 12:47:45 -0700

    changes in class starting to add web simulation

---
+++ BlazorServerWebSimulation/SimulationNode.cs
@@ -0,0 +1,24 @@
+//ctrl + shift + p -> reload window
+
+//TODO: should implement INode, not node
+using library;
+namespace Simulation;
+public class SimulationNode: Server {
+    public readonly SimulationNode InnerNode;
+    public SimulationNode(Server node) {
+        this.InnerNode = node;
+    }
+
+    public int Id {get => InnerNode.Id; set => InnerNode.Id = value;}
+    public Server? Leader {get => InnerNode.RecognizedLeader; set => InnerNode.RecognizedLeader = value;}
+    //public int Leader {get => InnerNode.Leader; set => InnerNode.Leader = value;}
+
+    public States state { get => InnerNode.state; set => InnerNode.state = value; }
+
+    //And I would continue with other node properties
+
+    // public Dictionary<int, bool> AppendEntriesResponseLog = new();
+    // public int CurrentTerm { get; set; }
+
+    // public Stopwatch timeSinceHearingFromLeader = new();
+}
