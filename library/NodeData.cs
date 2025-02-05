4bfe90133f7f1f4e3d261d11ca3717983d7ba4e7
reall <reall@MSI>
2025-02-05 13:48:51 -0700

    som emessage

---
+++ library/NodeData.cs
@@ -0,0 +1,13 @@
+namespace library;
+
+public class NodeData() {
+    public int Id {get;set;}
+    public string? Status {get;set;}
+    public int ElectionTimeout {get;set;}
+    public int Term {get;set;}
+    public IServer? CurrentTermLeader {get;set;}
+    public int CommittedEntryIndex {get;set;}
+    public IEnumerable<RaftLogEntry> Log {get;set;}
+    public States State {get;set;}
+    public int NodeIntervalScalar {get;set;}
+};
